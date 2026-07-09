c454383f5cbb076d6f01e7be7cbb41dd7f12e65c
codet834290349820 <julius.lenger@gmail.com>
2026-07-09 14:21:27 +0200

    einfach Funktionen aus ein paar Dingen gemacht

--- ZweiterVersuch.py
+++ ZweiterVersuch.py
@@ -21,39 +21,44 @@ print(f"data3: Mittelwert = {mu3:.3f}, Standardabweichung = {sigma3:.3f}")
 data = np.stack([data1, data2, data3])
 
 
+def zeige_histogramme():
+    plt.hist(data1, bins=30, density=True, alpha=0.5)
+    plt.hist(data2, bins=30, density=True, alpha=0.5)
+    plt.hist(data3, bins=30, density=True, alpha=0.5)
 
-plt.hist(data1, bins=30, density=True, alpha=0.5)
-plt.hist(data2, bins=30, density=True, alpha=0.5)
-plt.hist(data3, bins=30, density=True, alpha=0.5)
 
-
-plt.xlabel("Wert")
-plt.ylabel("Wahrscheinlichkeit")
-plt.title("Normalverteilung")
-plt.show()
+    plt.xlabel("Wert")
+    plt.ylabel("Wahrscheinlichkeit")
+    plt.title("Normalverteilung")
+    plt.show()
 
 print("Kovarianzmatrix:")
 print(np.round(np.cov(data), 3))
 
-fig = go.Figure(data=[go.Scatter3d(
-    x=data1, y=data2, z=data3,
-    mode='markers',
-    marker=dict(
-        size=4,
-        color=data3,                # Färbe die Punkte basierend auf der z-Achse
-        colorscale='Viridis',   # Farbskala (z.B. Viridis, Plasma)
-        opacity=0.8
-    )
-)])
 
-# 3. Layout anpassen
-fig.update_layout(
-    title='Interaktive 3D-Gauß-Wolke',
-    scene=dict(
-        xaxis_title='X-Achse',
-        yaxis_title='Y-Achse',
-        zaxis_title='Z-Achse'
-    )
-)
+def erstellemodell3D():
+    fig = go.Figure(data=[go.Scatter3d(
+        x=data1, y=data2, z=data3,
+        mode='markers',
+        marker=dict(
+            size=4,
+            color=data3,                # Färbe die Punkte basierend auf der z-Achse
+            colorscale='Viridis',   # Farbskala (z.B. Viridis, Plasma)
+            opacity=0.8
+        )
+    )])
 
-fig.show()
+    # 3. Layout anpassen
+    fig.update_layout(
+        title='Interaktive 3D-Gauß-Wolke',
+        scene=dict(
+            xaxis_title='X-Achse',
+            yaxis_title='Y-Achse',
+            zaxis_title='Z-Achse'
+        )
+    )
+
+    fig.show()
+
+zeige_histogramme()
+erstellemodell3D()
